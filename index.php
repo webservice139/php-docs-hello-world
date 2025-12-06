1c6499d90a7ab940970851b0c3540c67b5f8f4d0
webservice139 <web.service139@gmail.com>
2025-12-06 03:55:47 +0000

    revision to html

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-
-echo "Hello CMVIP 視障人士福音中心 on Azure!";
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello 視障人士福音中心 on Azure!</p>'; ?>
+    </body>
+</html>
